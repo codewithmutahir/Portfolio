09d72344a377c9bed827c72ee41d667890c21715
codewithmutahir <mutharsoomro13@gmail.com>
2024-10-20 14:46:43 +0500

    shadcn library added

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,19 @@
  *  @license   Apache-2.0
  */
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
 /**
  * CSS links
  */
 
-import 'lenis/dist/lenis.css'
-import './index.css'
+import "lenis/dist/lenis.css";
+import "./index.css";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+      <App />
+  </StrictMode>
+);
